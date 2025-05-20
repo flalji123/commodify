cc31e227a5456c573b262e0220ec5551ad585d41
Farid Lalji <flalji@learn-arena.co.uk>
2025-05-20 12:02:38 +0400

    fix: use correct PORT env variable

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Move into backend directory if needed (only if main.py is inside it)
 cd backend
+
+# Install dependencies
 pip install -r requirements.txt
-uvicorn main:app --host 0.0.0.0 --port $PORT
+
+# Start FastAPI with the PORT environment variable
+uvicorn main:app --host 0.0.0.0 --port ${PORT}
